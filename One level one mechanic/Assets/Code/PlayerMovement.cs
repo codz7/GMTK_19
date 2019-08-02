eed6db06e65a521d4e2e7c4b28b357010c3669be
Olle Hagman <codz57@gmail.com>
2019-08-03 00:18:03 +0200

    add physics movement

--- One level one mechanic/Assets/Code/PlayerMovement.cs
+++ One level one mechanic/Assets/Code/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            rb.velocity = -Vector2.right;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb.velocity = Vector2.right;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rb.velocity = (Vector2.up);
+        }
     }
 }
