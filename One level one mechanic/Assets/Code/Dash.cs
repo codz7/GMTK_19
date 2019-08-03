fa5e2aff61503c8cc63bdc7f4572ab6d8fbc79e5
Olle Hagman <codz57@gmail.com>
2019-08-03 18:52:51 +0200

    started on a dash

---
+++ One level one mechanic/Assets/Code/Dash.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Dash : PlayerMovement
+{
+    public Vector3 dashVelocity;
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(Input.GetButtonDown("fire1"))
+        {
+            //dashVelocity = 
+        }
+    }
+}
