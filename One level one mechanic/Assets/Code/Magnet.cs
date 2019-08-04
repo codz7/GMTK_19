8652a39105c8ed604b8ec3bb80076bfaf128730d
Olle Hagman <codz57@gmail.com>
2019-08-04 19:51:38 +0200

    be4tter hook and magnet

---
+++ One level one mechanic/Assets/Code/Magnet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    public Vector2 direction;
+    bool hasColided;
+
+    private void Update()
+    {
+        if (!hasColided)
+        {
+            transform.Translate(20f * direction * Time.deltaTime);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string tag = collision.gameObject.tag;
+
+        if (tag == "Player")
+        {
+            collision.gameObject.GetComponent<Hookshot>().hooking = false;
+        }
+        else if (tag == "Spikes")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            hasColided = true;
+        }
+    }
+}
